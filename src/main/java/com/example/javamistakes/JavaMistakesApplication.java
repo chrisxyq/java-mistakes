daa94b463df47e79d5c3c6dff544b6657a8c0427
chrisxyq <yuanqixu@trip.com>
2022-10-08 10:59:33 +0800

    Java业务开发常见错误100例

---
+++ src/main/java/com/example/javamistakes/JavaMistakesApplication.java
@@ -0,0 +1,14 @@
+package com.example.javamistakes;
+
+import com.example.javamistakes.concurrenttool.threadlocal.CommonMistakesApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMistakesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMistakesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javamistakes/JavaMistakesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javamistakes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMistakesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
